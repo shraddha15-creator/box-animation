736fdd407f70c83c73d759546773832aaeffd941
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-08-29 22:14:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const boxOne = document.querySelector('.box-one');
+const boxTwo = document.querySelector('.box-two');
+const boxThree = document.querySelector('.box-three');
+const boxFour = document.querySelector('.box-four');
+const boxFive = document.querySelector('.box-five');
+
+const startBtn = document.querySelector('#btn-start');
+const stopBtn = document.querySelector('#btn-stop');
+
+startBtn.addEventListener('click', () => {
+    
+})
